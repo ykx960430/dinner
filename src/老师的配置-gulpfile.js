321aa340c6a2b4fd5ba056bf250a078d4863181e
Kyle <1437227057@qq.com>
2018-03-08 21:04:37 +0800

    写完头部到楼层
    写完头部到楼层

--- src/老师的配置-gulpfile.js
+++ src/老师的配置-gulpfile.js
@@ -6,26 +6,26 @@
 
 
 // 引入使用到的模块 插件
-let gulp = require("gulp"),
-    minifyCss = require("gulp-clean-css"),
+var gulp = require("gulp"),
     uglify = require("gulp-uglify"),
-    htmlmin = require("gulp-htmlmin"),
     sass = require("gulp-sass"),
-    babel = require("gulp-babel"),
     connect = require("gulp-connect");
-    _root = "src";
+    htmlmin = require("gulp-htmlmin"),
+    minifyCss = require("gulp-clean-css"),
+    babel = require("gulp-babel");
+
 
 // 01 启动服务器
 gulp.task("server", function(){
     connect.server({
-        root : _root,
+        root : "dist",
         livereload : true
     });
 });
 
 // 02 定义 gulp 任务, 压缩CSS
 gulp.task("css", function(){
-    gulp.src("src/css/*.css")
+    gulp.src("css/**/*.css")
         .pipe(minifyCss())
         .pipe(gulp.dest("dist/css"))
         .pipe(connect.reload());
@@ -33,10 +33,10 @@ gulp.task("css", function(){
 
 // 03 定义 gulp 任务，压缩JS
 gulp.task("js", function(){
-    gulp.src("src/js/**/*.js")
-        .pipe(babel({
-            presets: ['env']
-        }))
+    gulp.src("js/**/*.js")
+        //.pipe(babel({
+        //    presets: ['env']
+        //}))
         .pipe(uglify())
         .pipe(gulp.dest("dist/js"))
         .pipe(connect.reload());
@@ -44,7 +44,13 @@ gulp.task("js", function(){
 
 // 04 定义 gulp 任务，压缩 HTML
 gulp.task("html", function(){
-    gulp.src(["src/**/*.html"])
+    gulp.src("**/*.html")
+        .pipe(htmlmin({collapseWhitespace: true, minifyCSS:true, minifyJS:true}))
+        .pipe(gulp.dest("dist"))
+        .pipe(connect.reload());
+});
+gulp.task("htmlIndex", function(){
+    gulp.src("*.html")
         .pipe(htmlmin({collapseWhitespace: true, minifyCSS:true, minifyJS:true}))
         .pipe(gulp.dest("dist"))
         .pipe(connect.reload());
@@ -52,35 +58,36 @@ gulp.task("html", function(){
 
 // 05 定义任务，编译 sass
 gulp.task("sass", function(){
-    gulp.src("src/sass/**/*.scss")
+    gulp.src("sass/**/*.scss")
         .pipe(sass({outputStyle:"compressed"}))
-        .pipe(gulp.dest(_root + "/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(connect.reload());
 });
 
 // 06 将图片、库、模拟的假数据复制到 dist 下
 gulp.task("images", function(){
-    gulp.src("src/images/**/*.*")
+    gulp.src("images/**/*.*")
         .pipe(gulp.dest("dist/images"));
 });
 gulp.task("lib", function(){
-    gulp.src("src/lib/**/*.*")
+    gulp.src("lib/**/*.*")
         .pipe(gulp.dest("dist/lib"));
 });
 gulp.task("mock", function(){
-    gulp.src("src/mock/**/*.*")
+    gulp.src("mock/**/*.*")
         .pipe(gulp.dest("dist/mock"));
 });
 gulp.task("copyfile", ["images", "lib", "mock"]);
 
 // 07 定义任务，监视sass 、css、html、js
 gulp.task("watch", function(){
-    gulp.watch("src/sass/**/*.scss", ["sass"]);
-    gulp.watch("src/css/**/*.css", ["css"]);
-    gulp.watch("src/**/*.html", ["html"]);
-    gulp.watch("src/js/**/*.js", ["js"]);
+    gulp.watch("sass/**/*.scss", ["sass"]);
+    gulp.watch("css/**/*.css", ["css"]);
+    gulp.watch("html/**/*.html", ["html"]);
+    gulp.watch("*.html", ["htmlIndex"]);
+    gulp.watch("js/**/*.js", ["js"]);
 })
 
 // 08 定义默认任务
-gulp.task("default", ["js","css", "html", "sass","copyfile","server", "watch"]);
+gulp.task("default", ["js", "html", "sass","htmlIndex", "server", "copyfile", "watch"]);
 
