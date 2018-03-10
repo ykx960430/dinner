88ab5577ff53cd018570a68bd4958d964c5b92a5
Kyle <1437227057@qq.com>
2018-03-10 17:50:22 +0800

    Revert "不知道该如何判断元素是否在可视区"
    This reverts commit 556c4f9d6e1dfc7934423851cefc655212b8bb51.

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2018-03-08 09:49:39
 * @Last Modified by:   Marte
-* @Last Modified time: 2018-03-10 17:40:55
+* @Last Modified time: 2018-03-10 14:49:05
 */
 
 'use strict';
@@ -31,7 +31,7 @@ require(["config"], function(){
         /*
          * 滚动触发元素显示隐藏
          *     搜索栏吸顶
-         *     鼠标滑过显示二级菜单
+         *
          *
          */
         // 搜索栏吸顶
@@ -64,71 +64,8 @@ require(["config"], function(){
         });
 
 
-        /*
-         *  实现 导航 页面内跳转
-         *  回到顶部
-         *
-         */
-        $(function(){
-            $(".sideNav").on('click', '> li', function(event) {
-                event.preventDefault();
-                // 给当前的元素加上类名
-                $(".sideNav>li").each(function(){
-                    $(this).removeClass('curren');
-                })
-                $(this).addClass('curren');
-                $(".floorHeader").each(function(){
-                    $(this).removeClass("curren");
-                })
-                $($(".floorHeader")[$(this).index()]).addClass('curren');
 
-                var scrollToWhere =  $($(".floorHeader")[$(this).index()]).offset().top - 130 + "px";
-                $("body").animate({
-                    scrollTop: scrollToWhere},
-                    300, function() {
-                });
-            });
-            // allCate
-            $(".allCate").on('click', '> li', function(event) {
-                event.preventDefault();
-                // 给当前的元素加上类名
-                $(".allCate>li").each(function(){
-                    $(this).removeClass('curren');
-                })
-                $(this).addClass('curren');
-                $(".floorHeader").each(function(){
-                    $(this).removeClass("curren");
-                })
-                $($(".floorHeader")[$(this).index()]).addClass('curren');
 
-                var scrollToWhere =  $($(".floorHeader")[$(this).index()]).offset().top - 130 + "px";
-                $("body").animate({
-                    scrollTop: scrollToWhere},
-                    300, function() {
-                });
-            });
-
-            // 页面滚动时 判断对应的nav.curre显示
-            $(window).scroll(function(event) {
-                var hideHeight = $(document).scrollTop();
-                var nowArr =[];
-                $(".floorHeader").each(function(){
-                    // 判断当前谁在可视区范围
-                    // 元素顶端到可见区域顶端的距离  <= 浏览器可见区域高度
-                    // console.log($(this).offset().top, $(window).height());
-                        if( $(this).offset().top >= $(window).scrollTop() && $(this).offset().top < ($(window).scrollTop()+$(window).height() ) ){
-                            // 不知道该如何判断
-                            nowArr.push(Number($(this).attr("data-id")));
-                            console.log(nowArr);
-                        }
-                })
-            });
-
-            //回到顶部
-            $(".goToTop").click(function(event) {
-                $("body").animate({scrollTop: 0 },300)
-            });
-        })
 
 
 
